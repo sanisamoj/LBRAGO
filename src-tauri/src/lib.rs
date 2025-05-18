8b51a221efcc805f9fba6d170bbbe0ab8b3d0a4d
sanisamoj <marioartec39@gmail.com>
2025-05-18 19:34:18 -0300

    single application

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -128,7 +128,18 @@ async fn regenerate_svk_to_member(arg: String) -> Result<String, String> {
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
-    tauri::Builder::default()
+    let mut builder = tauri::Builder::default();
+
+     #[cfg(desktop)]
+    {
+        builder = builder.plugin(tauri_plugin_single_instance::init(|app, args, cwd| {
+            let _ = app.get_webview_window("main")
+                       .expect("no main window")
+                       .set_focus();
+        }));
+    }
+
+    builder
         .plugin(tauri_plugin_process::init())
         .plugin(tauri_plugin_updater::Builder::new().build())
         .plugin(tauri_plugin_clipboard_manager::init())
--- src/Config.ts
+++ src/Config.ts
@@ -1,7 +1,7 @@
 export class Config {
     public static API_URL = "https://lembrago.sanisamojrepository.com/api"
     public static FAVICON_URL = `${Config.API_URL}/favicon.ico`
-    public static VERSION = "0.8.2"
+    public static VERSION = "0.8.3"
 
     static token: string
 
